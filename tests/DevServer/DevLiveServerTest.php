aafee331e4c0ef162d18e4636273be1dcb1e41b7
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-01-12 20:20:10 +0100

    Add UnitTest Channel. Defined a Channel who can run Test. Its don't destroy the hole Teamspeak at errors. Delete unnecessary Code Lines

--- tests/DevServer/DevLiveServerTest.php
+++ tests/DevServer/DevLiveServerTest.php
@@ -21,10 +21,9 @@ class DevLiveServerTest extends TestCase
     private string $queryPort;
     private string $user;
     private string $password;
-
-    private string $original_ssh_timeout;
-    private string $original_execution_timeout;
     private string $ts3_server_uri;
+    private string $ts3_unit_test_channel_name;
+    private int $test_cid;
 
     public function setUp(): void
     {
@@ -37,6 +36,7 @@ class DevLiveServerTest extends TestCase
             $this->queryPort = str_replace('DEV_LIVE_SERVER_QUERY_PORT=', '', preg_replace('#\n(?!\n)#', '', $env[4]));
             $this->user = str_replace('DEV_LIVE_SERVER_QUERY_USER=', '', preg_replace('#\n(?!\n)#', '', $env[5]));
             $this->password = str_replace('DEV_LIVE_SERVER_QUERY_USER_PASSWORD=', '', preg_replace('#\n(?!\n)#', '', $env[6]));
+            $this->ts3_unit_test_channel_name = str_replace('DEV_LIVE_SERVER_UNIT_TEST_CHANNEL=', '', preg_replace('#\n(?!\n)#', '', $env[7]));
 
         } else {
             $this->active = "false";
@@ -49,11 +49,6 @@ class DevLiveServerTest extends TestCase
             '&blocking=0'.
             '&timeout=30'.
             '&nickname=UnitTestBot';
-
-        $this->original_ssh_timeout = ini_get('default_socket_timeout');
-        $this->original_execution_timeout = ini_get('max_execution_time');
-        ini_set('default_socket_timeout', 2);
-        ini_set('max_execution_time', 10);
     }
 
     /**
@@ -61,7 +56,7 @@ class DevLiveServerTest extends TestCase
      * @throws ServerQueryException
      * @throws HelperException
      */
-    public function test_ssh_connection()
+    public function test_can_connect()
     {
         if ($this->active == "false") {
             $this->markTestSkipped('DevLiveServer ist not active');
@@ -71,12 +66,115 @@ class DevLiveServerTest extends TestCase
         $ts3_VirtualServer = TeamSpeak3::factory($this->ts3_server_uri);
         $nodeInfo = $ts3_VirtualServer->getInfo();
 
+        $ts3_VirtualServer->getAdapter()->getTransport()->disconnect();
         $this->assertEquals('Linux', $nodeInfo["virtualserver_platform"]);
     }
 
-    public function tearDown(): void
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws HelperException
+     */
+    public function test_can_get_channel_info()
     {
-        ini_set('default_socket_timeout', $this->original_ssh_timeout);
-        ini_set('max_execution_time', $this->original_execution_timeout);
+        if ($this->active == "false") {
+            $this->markTestSkipped('DevLiveServer ist not active');
+        }
+
+        $ts3_VirtualServer = TeamSpeak3::factory($this->ts3_server_uri);
+        $channelInfo = $ts3_VirtualServer->channelGetByName($this->ts3_unit_test_channel_name)->getInfo();
+
+        $ts3_VirtualServer->getAdapter()->getTransport()->disconnect();
+
+        $this->assertEquals($this->ts3_unit_test_channel_name, $channelInfo["channel_name"]);
+        $this->assertEquals(1, $channelInfo["channel_flag_permanent"]);
+        $this->assertEquals("-1", $channelInfo["channel_maxclients"]);
+        $this->assertEquals("-1", $channelInfo["channel_maxfamilyclients"]);
+
+    }
+
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws HelperException
+     */
+    public function test_can_create_play_test_channel()
+    {
+        if ($this->active == "false") {
+            $this->markTestSkipped('DevLiveServer ist not active');
+        }
+
+        $ts3_VirtualServer = TeamSpeak3::factory($this->ts3_server_uri);
+        $cid = $this->set_play_test_channel($ts3_VirtualServer);
+        $cidTest = $ts3_VirtualServer->channelGetById($cid)->getInfo();
+
+        $this->assertNotNull($cid);
+        $this->assertEquals('Play-Test', $cidTest["channel_name"]);
+
+        $this->unset_play_test_channel($ts3_VirtualServer);
+
+        $ts3_VirtualServer->getAdapter()->getTransport()->disconnect();
+    }
+
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws HelperException
+     */
+    public function test_can_create_standard_channel()
+    {
+        if ($this->active == "false") {
+            $this->markTestSkipped('DevLiveServer ist not active');
+        }
+
+        $ts3_VirtualServer = TeamSpeak3::factory($this->ts3_server_uri);
+        $this->set_play_test_channel($ts3_VirtualServer);
+
+        $testCid = $ts3_VirtualServer->channelCreate([
+            "channel_name" => 'Standard Channel',
+            "channel_codec" => 4,
+            "channel_codec_quality" => 6,
+            "channel_flag_semi_permanent" => 0,
+            "channel_flag_permanent" => 1,
+            "cpid" => $this->test_cid,
+        ]);
+        $testCidResult = $ts3_VirtualServer->channelGetById($testCid)->getInfo();
+
+        $this->assertEquals('Standard Channel', $testCidResult["channel_name"]);
+        $this->assertEquals(4, $testCidResult["channel_codec"]);
+        $this->assertEquals(6, $testCidResult["channel_codec_quality"]);
+        $this->assertEquals(0, $testCidResult["channel_flag_semi_permanent"]);
+        $this->assertEquals(1, $testCidResult["channel_flag_permanent"]);
+
+        $this->unset_play_test_channel($ts3_VirtualServer);
+        $ts3_VirtualServer->getAdapter()->getTransport()->disconnect();
+    }
+
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws HelperException
+     */
+    private function set_play_test_channel($ts3VirtualServer): int
+    {
+        $cid = $ts3VirtualServer->channelGetByName($this->ts3_unit_test_channel_name)->getInfo();
+
+        $createdCID = $ts3VirtualServer->channelCreate([
+            "channel_name" => 'Play-Test',
+            'channel_flag_permanent' => 1,
+            "cpid" => $cid['cid'],
+        ]);
+        $this->test_cid = $createdCID;
+        return $createdCID;
+    }
+
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws HelperException
+     */
+    public function unset_play_test_channel($ts3_VirtualServer): void
+    {
+        $ts3_VirtualServer->channelDelete($this->test_cid, true);
     }
 }
