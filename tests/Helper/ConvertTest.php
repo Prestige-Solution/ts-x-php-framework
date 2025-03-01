0c61858435c2b454ffbba65121f6b516677f26fd
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-03-01 22:01:53 +0100

    change: define convert seconds more readable

--- tests/Helper/ConvertTest.php
+++ tests/Helper/ConvertTest.php
@@ -150,11 +150,11 @@ class ConvertTest extends TestCase
         $this->assertEquals('0D 00:01:00', $output);
         $this->assertIsString($output);
 
-        $output = Convert::seconds(strtotime('59 minutes 59 seconds', 0));
+        $output = Convert::seconds((59 * 60) + 59);
         $this->assertEquals('0D 00:59:59', $output);
         $this->assertIsString($output);
 
-        $output = Convert::seconds(strtotime('1 hours', 0));
+        $output = Convert::seconds((59 * 60) + 60);
         $this->assertEquals('0D 01:00:00', $output);
         $this->assertIsString($output);
 
@@ -198,7 +198,7 @@ class ConvertTest extends TestCase
         $this->assertEquals('-0D 01:00:00', $output);
         $this->assertIsString($output);
 
-        $output = Convert::seconds(strtotime('23 hours 59 minutes 59 seconds ago', 0));
+        $output = Convert::seconds( strtotime('23 hours 59 minutes 59 seconds ago', 0));
         $this->assertEquals('-0D 23:59:59', $output);
         $this->assertIsString($output);
 
