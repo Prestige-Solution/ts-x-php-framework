5531e1a7dfa164dabf262490fce6a34c205d8f87
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-10-11 15:25:20 +0200

    remove unnecessary param

--- tests/DevLiveServer/ChannelTest.php
+++ tests/DevLiveServer/ChannelTest.php
@@ -54,7 +54,6 @@ class ChannelTest extends TestCase
         $this->ts3_server_uri = 'serverquery://'.$this->user.':'.$this->password.'@'.$this->host.':'.$this->queryPort.
             '/?server_port=9987'.
             '&no_query_clients=0'.
-            '&blocking=0'.
             '&timeout=30';
     }
 
