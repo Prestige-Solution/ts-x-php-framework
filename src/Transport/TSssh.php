44d8896e25cdc8ca228beca7429428dc4c12a96a
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-10-13 22:33:39 +0200

    during migration tests from ts3 to ts6, only with channels and permission, it occurred that we need massively more runtime

--- src/Transport/TSssh.php
+++ src/Transport/TSssh.php
@@ -19,7 +19,8 @@ class TSssh extends Transport
     {
         $this->ssh = new SSH2($this->config['host'], $this->config['port']);
 
-        $this->ssh->setTimeout($this->config['timeout']);
+        $this->stream = $this->ssh->setTimeout($this->config['timeout']);
+        stream_set_timeout($this->stream, $this->config['timeout']);
         $this->ssh->setPreferredAlgorithms([
             'hostkey' => ['rsa-sha2-512', 'rsa-sha2-256', 'ssh-rsa'],
         ]);
