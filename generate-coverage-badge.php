9ad37902aaace7f0a03153638b204e1eeab626a2
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-10-19 19:30:28 +0200

    code-style

--- generate-coverage-badge.php
+++ generate-coverage-badge.php
@@ -1,13 +1,14 @@
 <?php
+
 $cloverFile = '.phpunit.cache/clover.xml';
 $outputSvg = 'doc/coverage/coverage-badge.svg';
 
-if (!file_exists($cloverFile)) {
+if (! file_exists($cloverFile)) {
     die("Clover file not found: $cloverFile\n");
 }
 
 $clover = simplexml_load_file($cloverFile);
-if (!isset($clover->project->metrics)) {
+if (! isset($clover->project->metrics)) {
     die("No project-level metrics found in Clover XML\n");
 }
 
@@ -18,17 +19,17 @@ $covered = (int) $metrics['coveredelements'];
 $coverage = $elements > 0 ? round($covered / $elements * 100) : 0;
 
 if ($coverage >= 90) {
-    $color = "#4c1";
+    $color = '#4c1';
 } elseif ($coverage >= 75) {
-    $color = "#dfb317";
+    $color = '#dfb317';
 } elseif ($coverage >= 50) {
-    $color = "#fe7d37";
+    $color = '#fe7d37';
 } else {
-    $color = "#e05d44";
+    $color = '#e05d44';
 }
 
-$label = "coverage";
-$value = $coverage . "%";
+$label = 'coverage';
+$value = $coverage.'%';
 $labelWidth = 60;
 $valueWidth = 40;
 $height = 20;
@@ -36,7 +37,6 @@ $height = 20;
 $totalWidth = $labelWidth + $valueWidth;
 $labelX = $labelWidth / 2;
 $valueX = $labelWidth + ($valueWidth / 2);
-
 
 $svg = <<<SVG
 <svg xmlns="http://www.w3.org/2000/svg" width="{$totalWidth}" height="{$height}">
@@ -59,8 +59,6 @@ $svg = <<<SVG
   </g>
 </svg>
 SVG;
-
-
 
 file_put_contents($outputSvg, $svg);
 echo "Badge created: $outputSvg ($coverage%)\n";
