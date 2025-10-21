2aabe2216d18b8ed1722eeee94e15b49937b07a5
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2025-10-21 17:19:07 +0200

    enhance test to validate fix a type issue at clientGetByName

--- tests/DevLiveServer/ClientTest.php
+++ tests/DevLiveServer/ClientTest.php
@@ -157,6 +157,10 @@ class ClientTest extends TestCase
         $clientChannelByID = $ts3_VirtualServer->channelGetById($testCid)->clientGetById($userID);
         $this->assertEquals($this->ts3_unit_test_userName, $clientChannelByID['client_nickname']);
 
+        //get client by channel clientGetByName
+        $clientChannelByName = $ts3_VirtualServer->channelGetById($testCid)->clientGetByName($this->ts3_unit_test_userName);
+        $this->assertEquals($this->ts3_unit_test_userName, $clientChannelByName['client_nickname']);
+
         $this->unset_play_test_channel($ts3_VirtualServer);
         $ts3_VirtualServer->getAdapter()->getTransport()->disconnect();
     }
