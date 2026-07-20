77e5940a02f4f26af220a4d148ff7fee596bdaa5
Oliver Nitzsche <onitzsche@prestige-solutions.de>
2026-07-20 18:58:48 +0200

    add: FileTransferTest class for testing

---
+++ tests/DevLiveServer/FIleTransferTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace PlanetTeamSpeak\TeamSpeak3Framework\Tests\DevLiveServer;
+
+use PHPUnit\Framework\TestCase;
+use PlanetTeamSpeak\TeamSpeak3Framework\Exception\AdapterException;
+use PlanetTeamSpeak\TeamSpeak3Framework\Exception\ServerQueryException;
+use PlanetTeamSpeak\TeamSpeak3Framework\Exception\TransportException;
+use PlanetTeamSpeak\TeamSpeak3Framework\TeamSpeak3;
+
+class FIleTransferTest extends TestCase
+{
+    /**
+     * ATTENTION
+     * Use the .env.testing Variable "DEV_LIVE_SERVER_AVAILABLE" to activate this Test
+     * Use this Testcase only with a development Teamspeak Server
+     * Otherwise the TS3 Server can be destroyed
+     */
+    private string $active;
+
+    private string $host;
+
+    private string $queryPort;
+
+    private string $user;
+
+    private string $password;
+
+    private string $ts3_server_uri;
+
+    public function setUp(): void
+    {
+        //proof test active
+        if (file_exists('./.env.testing')) {
+            $env = file('./.env.testing');
+            //get live server is available
+            $this->active = str_replace('DEV_LIVE_SERVER_AVAILABLE=', '', preg_replace('#\n(?!\n)#', '', $env[2]));
+            $this->host = str_replace('DEV_LIVE_SERVER_HOST=', '', preg_replace('#\n(?!\n)#', '', $env[3]));
+            $this->queryPort = str_replace('DEV_LIVE_SERVER_QUERY_PORT=', '', preg_replace('#\n(?!\n)#', '', $env[4]));
+            $this->user = str_replace('DEV_LIVE_SERVER_QUERY_USER=', '', preg_replace('#\n(?!\n)#', '', $env[5]));
+            $this->password = str_replace('DEV_LIVE_SERVER_QUERY_USER_PASSWORD=', '', preg_replace('#\n(?!\n)#', '', $env[6]));
+        } else {
+            $this->active = 'false';
+        }
+
+        $this->ts3_server_uri = 'serverquery://'.$this->user.':'.$this->password.'@'.$this->host.':'.$this->queryPort.
+            '/?server_port=9987'.
+            '&no_query_clients=0'.
+            '&blocking=0'.
+            '&timeout=30';
+    }
+
+    /**
+     * @throws AdapterException
+     * @throws ServerQueryException
+     * @throws TransportException
+     * @throws \Exception
+     */
+    public function test_can_upload_download(): void
+    {
+        if ($this->active == 'false') {
+            $this->markTestSkipped('DevLiveServer ist not active');
+        }
+
+        $ts3_Host = TeamSpeak3::factory($this->ts3_server_uri);
+
+        $channel = $ts3_Host->channelGetByName('UnitTest');
+        $channel->fileUpload('/test.txt', 'Hallo Welt', overwrite: true);
+        $content = $channel->fileDownload('/test.txt');
+
+        $this->assertEquals('Hallo Welt', $content->toString());
+
+        $channel->fileDelete('', '/test.txt');
+        $ts3_Host->getAdapter()->getTransport()->disconnect();
+    }
+
+}
